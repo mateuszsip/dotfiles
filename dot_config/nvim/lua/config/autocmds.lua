a0fa6dd1834c48a6ea2d04f3f5fba1c158029e15
Mateusz Sip <mateusz.sip@gmail.com>
2026-04-29 19:38:06 +0200

    Neovim update

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.schedule(function()
+      vim.cmd("cclose")
+      require("trouble").open({ mode = "quickfix", focus = true, open_folds = false })
+    end)
+  end,
+})
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -32,6 +32,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>k", vim.tbl_extend("force", opts, { desc = "Go to Upper Window" }))
     vim.keymap.set("t", "<C-;>", "<C-\\><C-n><C-w>l", vim.tbl_extend("force", opts, { desc = "Go to Right Window" }))
     vim.keymap.set("t", "<C-h>", "<C-\\><C-n>", vim.tbl_extend("force", opts, { desc = "Exit Terminal Mode" }))
+    vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", vim.tbl_extend("force", opts, { desc = "Exit Terminal Mode" }))
     -- Pass <C-f> through to terminal (for opencode)
     vim.keymap.set("t", "<C-f>", "<C-f>", vim.tbl_extend("force", opts, { desc = "Pass to terminal" }))
   end,
