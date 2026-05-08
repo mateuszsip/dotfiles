c7d7a743c3a9283965fd00fac5ab900c9ff05499
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-08 13:58:26 +0200

    nvim: disable render-markdown wiki link icon to fix list indentation

---
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    link = {
+      -- Remove wiki link icon to fix indentation alignment in lists.
+      -- The globe icon adds width that misaligns sub-list items.
+      wiki = {
+        icon = "",
+      },
+    },
+  },
+}
