7d8ac0bf1b5cdc60cd3aad4f5790e03e98dc17ec
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-08 17:43:04 +0200

    nvim: revert render-markdown to defaults, clear icon-fighting code

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -1,6 +1,3 @@
--- render-markdown link icon and YAML settings are patched directly in
--- after/ftplugin/markdown.lua, which runs after the plugin's config function
--- regardless of lazy.nvim spec merging order.
 return {
   "MeanderingProgrammer/render-markdown.nvim",
 }
