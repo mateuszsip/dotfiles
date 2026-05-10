cc62aeb85fe7decfdcb0d03ec8f49560282029f0
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-10 22:22:00 +0200

    nvim use nushell

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
+
+-- Use nushell for :terminal; keep POSIX shell for plugin commands
+if vim.fn.executable("nu") == 1 then
+  vim.opt.shell = "nu"
+  vim.opt.shellcmdflag = "-c"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
