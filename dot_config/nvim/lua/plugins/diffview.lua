5389f2c6175a63ef32847199b0bc9c2cadf97ee0
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-12 11:45:51 +0200

    [nvim] remove diffview

---
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diff View (git)" },
+    { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+    { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Repo History" },
+  },
+}
