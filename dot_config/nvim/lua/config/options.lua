0989583798fff99b5a807fcd51365c38a56b3e06
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-12 10:59:26 +0200

    [nvim] scroll padding

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 3
 
 -- Use nushell for :terminal; keep POSIX shell for plugin commands
 if vim.fn.executable("nu") == 1 then
