3044eef678e0369d63db5c20270f4fbd44ce6473
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-15 18:13:35 +0200

    sidekick tweaks

--- dot_config/nvim/lua/plugins/sidekick.lua
+++ dot_config/nvim/lua/plugins/sidekick.lua
@@ -1,15 +1,33 @@
 return {
   "folke/sidekick.nvim",
   keys = {
-    { "<leader>ac", function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end, desc = "Sidekick Toggle Claude" },
-    { "<leader>ao", function() require("sidekick.cli").toggle({ name = "opencode", focus = true }) end, desc = "Sidekick Toggle Opencode" },
-    { "<A-;>", function() require("sidekick").nes_jump_or_apply() end, desc = "Goto/Apply Next Edit Suggestion" },
+    {
+      "<leader>ac",
+      function()
+        require("sidekick.cli").toggle({ name = "claude", focus = true })
+      end,
+      desc = "Sidekick Toggle Claude",
+    },
+    {
+      "<leader>ao",
+      function()
+        require("sidekick.cli").toggle({ name = "opencode", focus = true })
+      end,
+      desc = "Sidekick Toggle Opencode",
+    },
+    {
+      "<A-;>",
+      function()
+        require("sidekick").nes_jump_or_apply()
+      end,
+      desc = "Goto/Apply Next Edit Suggestion",
+    },
   },
   opts = {
     cli = {
       win = {
         split = {
-          width = 45,
+          width = 60,
         },
         float = {
           width = 0.75,
