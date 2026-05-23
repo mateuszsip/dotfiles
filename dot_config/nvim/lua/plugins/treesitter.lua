ee7b434d00ed1f34cdf73d9d204bdc45a8bf4671
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-23 11:30:26 +0200

    nvim: add treesitter ensure_installed list (php, yaml, bash, ini, xml, mermaid)

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "ini",
+      "mermaid",
+      "php",
+      "php_only",
+      "phpdoc",
+      "xml",
+      "yaml",
+    },
+  },
+}
