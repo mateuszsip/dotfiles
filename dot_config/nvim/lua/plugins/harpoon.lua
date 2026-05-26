81b648f85ffc10906216c3cbd24718fbe9945d76
Mateusz Sip <mateusz.sip@gmail.com>
2026-05-26 13:04:31 +0200

    feat(nvim): map harpoon quick menu to <leader>hh

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
         desc = "Harpoon File",
       },
       {
-        "<leader>h",
+        "<leader>hh",
         function()
           local harpoon = require("harpoon")
           harpoon.ui:toggle_quick_menu(harpoon:list())
