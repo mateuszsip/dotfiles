c603cf6c6e4375911cbe89b4b12a4109005e35bc
Mateusz Sip <mateusz.sip@gmail.com>
2026-06-02 09:41:54 +0200

    feat(nvim): prompt for dimensions on EasyTablesCreateNew keymap

--- dot_config/nvim/lua/plugins/easytables.lua
+++ dot_config/nvim/lua/plugins/easytables.lua
@@ -1,5 +1,21 @@
 return {
   "Myzel394/easytables.nvim",
+  ft = "markdown",
+  keys = {
+    {
+      "<leader>mn",
+      function()
+        vim.ui.input({ prompt = "Table dimensions (e.g. 3x3): " }, function(input)
+          if input and input ~= "" then
+            vim.cmd("EasyTablesCreateNew " .. input)
+          end
+        end)
+      end,
+      desc = "Markdown: New table",
+      ft = "markdown",
+    },
+    { "<leader>me", "<cmd>EasyTablesImportThisTable<cr>", desc = "Markdown: Edit table", ft = "markdown" },
+  },
   config = function()
     require("easytables").setup()
   end,
