5fb4185ea1cff7583c96c8c21be9425fbfbe055f
Mateusz Sip <mateusz.sip@gmail.com>
2026-06-13 14:48:35 +0200

    [nvim] videre

---
+++ dot_config/nvim/lua/plugins/videre.lua
@@ -0,0 +1,10 @@
+return {
+  "Owen-Dechow/videre.nvim",
+  cmd = "Videre",
+  dependencies = {
+    "Owen-Dechow/graph_view_yaml_parser",
+    "Owen-Dechow/graph_view_toml_parser",
+    "a-usr/xml2lua.nvim",
+  },
+  opts = {},
+}
