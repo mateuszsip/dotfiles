2d8e21adf3b8c73aab580e7c0b0e5220b7dc68cd
Mateusz Sip <mateusz.sip@gmail.com>
2026-07-12 22:15:05 +0200

    [nvim] close diffview easily

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -4,9 +4,19 @@ return {
   keys = {
     { "<leader>gdm", function() require("utils.git").diffview_against(false) end, desc = "Diff vs main/master" },
     { "<leader>gdM", function() require("utils.git").diffview_against(true) end, desc = "Diff vs origin/main/master" },
-    { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
     { "<leader>gdh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
     { "<leader>gdH", "<cmd>DiffviewFileHistory<cr>", desc = "Repo History" },
   },
-  opts = {},
+  opts = {
+    keymaps = {
+      view = {
+        ["q"] = "<cmd>DiffviewClose<cr>",
+        ["<esc>"] = "<cmd>DiffviewClose<cr>",
+      },
+      file_panel = {
+        ["q"] = "<cmd>DiffviewClose<cr>",
+        ["<esc>"] = "<cmd>DiffviewClose<cr>",
+      },
+    },
+  },
 }
