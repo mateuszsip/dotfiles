9990d2a76b88d79e7f855db69526b02970bc63c1
Mateusz Sip <mateusz.sip@gmail.com>
2026-07-12 22:20:24 +0200

    [nvim] add q/esc to close diffview

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -7,16 +7,21 @@ return {
     { "<leader>gdh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
     { "<leader>gdH", "<cmd>DiffviewFileHistory<cr>", desc = "Repo History" },
   },
+  config = function(_, opts)
+    require("diffview").setup(opts)
+    local close = function() vim.cmd("DiffviewClose") end
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "DiffviewFiles", "DiffviewFileHistory" },
+      callback = function(args)
+        vim.keymap.set("n", "<esc>", close, { buffer = args.buf, silent = true, desc = "Close Diffview" })
+        vim.keymap.set("n", "q", close, { buffer = args.buf, silent = true, desc = "Close Diffview" })
+      end,
+    })
+  end,
   opts = {
     keymaps = {
-      view = {
-        ["q"] = "<cmd>DiffviewClose<cr>",
-        ["<esc>"] = "<cmd>DiffviewClose<cr>",
-      },
-      file_panel = {
-        ["q"] = "<cmd>DiffviewClose<cr>",
-        ["<esc>"] = "<cmd>DiffviewClose<cr>",
-      },
+      view = { ["q"] = "<cmd>DiffviewClose<cr>", ["<esc>"] = "<cmd>DiffviewClose<cr>" },
+      file_panel = { ["q"] = "<cmd>DiffviewClose<cr>", ["<esc>"] = "<cmd>DiffviewClose<cr>" },
     },
   },
 }
