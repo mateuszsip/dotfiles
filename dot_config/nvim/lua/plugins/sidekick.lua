5df1cf5081120fd9f1ebcc854440617c3218c6fb
Mateusz Sip <mateusz.sip@gmail.com>
2026-07-21 22:35:51 +0200

    [nvim] fix sidekick mappings

--- dot_config/nvim/lua/plugins/sidekick.lua
+++ dot_config/nvim/lua/plugins/sidekick.lua
@@ -1,53 +1,61 @@
 return {
-  "folke/sidekick.nvim",
-  keys = {
-    {
-      "<leader>ac",
-      function()
-        require("sidekick.cli").toggle({ name = "claude", focus = true })
-      end,
-      desc = "Sidekick Toggle Claude",
-    },
-    {
-      "<leader>ao",
-      function()
-        require("sidekick.cli").toggle({ name = "opencode", focus = true })
-      end,
-      desc = "Sidekick Toggle Opencode",
-    },
-    {
-      "<leader>ap",
-      function()
-        require("sidekick.cli").toggle({ name = "pi", focus = true })
-      end,
-      desc = "Sidekick Toggle Pi",
-    },
-    {
-      "<leader>aP",
-      function()
-        require("sidekick.cli").toggle({ name = "antigravity", focus = true })
-      end,
-      desc = "Sidekick Toggle Antigravity",
-    },
-    {
-      "<A-;>",
-      function()
-        require("sidekick").nes_jump_or_apply()
-      end,
-      desc = "Goto/Apply Next Edit Suggestion",
-    },
-  },
-  opts = {
-    cli = {
-      win = {
-        split = {
-          width = 80,
-        },
-        float = {
-          width = 0.75,
-          height = 0.85,
-        },
-      },
-    },
-  },
+	"folke/sidekick.nvim",
+	keys = {
+		{
+			"<leader>ac",
+			function()
+				require("sidekick.cli").toggle({ name = "claude", focus = true })
+			end,
+			desc = "Sidekick Toggle Claude",
+		},
+		{
+			"<leader>ao",
+			function()
+				require("sidekick.cli").toggle({ name = "opencode", focus = true })
+			end,
+			desc = "Sidekick Toggle Opencode",
+		},
+		{
+			"<leader>ag",
+			function()
+				require("sidekick.cli").toggle({ name = "antigravity", focus = true })
+			end,
+			desc = "Sidekick Toggle Antigravity",
+		},
+		{
+			"<leader>ap",
+			function()
+				require("sidekick.cli").toggle({ name = "pi", focus = true })
+			end,
+			desc = "Sidekick Toggle Pi",
+		},
+		{
+			"<leader>aP",
+			function()
+				require("sidekick.cli").prompt()
+			end,
+			mode = { "n", "x" },
+			desc = "Sidekick Select Prompt",
+		},
+		{
+			"<A-;>",
+			function()
+				require("sidekick").nes_jump_or_apply()
+			end,
+			desc = "Goto/Apply Next Edit Suggestion",
+		},
+	},
+	opts = {
+		cli = {
+			win = {
+				split = {
+					width = 80,
+				},
+				float = {
+					width = 0.75,
+					height = 0.85,
+				},
+			},
+		},
+	},
 }
